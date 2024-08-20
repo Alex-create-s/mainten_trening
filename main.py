e81e4e484a43d9114a539c5d68bb2ce307c1ec5c
Alex-create-s <alexanderpu6@mail.ru>
2024-08-20 11:05:11 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,14 +3,8 @@ print(9**0.5*5)
 # 2nd program
 print(9.99>9.98 and 1000!=1000.1)
 #3pd program
-print(2*2-2)
+print(2*2+2)
 print(2*(2+2))
 print((2*2+2)==(2*(2+2)))
 # 4th program
-print("123.456")
-print(type("123.456"))
-print(type(float("123.456")))
-print(float("123.456")*10)
-print(float("1234.56")%10)
-print(int(4.55999999945))
 print(int(float("123.456")*10)%10)
